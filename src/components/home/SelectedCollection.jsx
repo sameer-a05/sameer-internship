59749625e70dcdfd2ea60a055b1e18f9777fe496
Sameer Alshamiri <sameer.alshamiri3@gmail.com>
2025-05-19 06:01:19 -0400

    Fixed skeleton loading state to SelectedCollection

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
-import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "../ui/Skeleton.css";
+import Skeleton from "../ui/Skeleton";
 
 export default function SelectedCollection() {
   const [data, setData] = useState(null);
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,9 +15,9 @@ export default function SelectedCollection() {
         const response = await axios.get(
           "https://remote-internship-api-production.up.railway.app/selectedCollection"
         );
-        setData(response.data);
-        setLoading(false)
-        console.log(response.data);
+        setData(response.data.data);
+        setLoading(false);
+        console.log(response.data.data);
       } catch (error) {
         console.log("Error fetching data:", error);
       }
@@ -28,50 +27,51 @@ export default function SelectedCollection() {
 
   return (
     <header>
-      <div className="selected-collection">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
-          className="selected-collection__bg"
-        />
-
-        {!loading ? (
+      {loading ? (
+        <div className="selected-collection">
+          <Skeleton  width="100%" height="100%" />
+        </div>
+      ) : (
+        <div className="selected-collection">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            poster={data?.thumbnail}
+            src={data?.videoLink}
+            className="selected-collection__bg"
+          />
           <div className="selected-collection__description">
             <img
-              src={data?.data?.logo}
+              src={data?.logo}
               alt=""
               className="selected-collection__logo"
             />
-            <h1 className="selected-collection__title">{data?.data?.title}</h1>
+            <h1 className="selected-collection__title">{data?.title}</h1>
             <Link
-              to={`/user/${data?.data?.creatorId}`}
+              to={`/user/${data?.creatorId}`}
               className="selected-collection__author"
             >
-              By {data?.data?.creator}
+              By {data?.creator}
               <img
                 src={VerifiedIcon}
                 className="selected-collection__author__verified"
               />
             </Link>
             <div className="selected-collection__details">
-              {data?.data?.amountOfItems} items · {data?.data?.floorPrice} ETH
+              {data?.amountOfItems} items · {data?.floorPrice} ETH
             </div>
             <Link
-              to={`/collection/${data?.data?.collectionId}`}
+              to={`/collection/${data?.collectionId}`}
               className="selected-collection__button"
             >
               <div className="green-pulse"></div>
               View Collection
             </Link>
           </div>
-        ) : (
-          <div className="selected-collection__description-skeleton"></div>
-        )}
-      </div>
+        </div>
+      )}
       ;
     </header>
   );
