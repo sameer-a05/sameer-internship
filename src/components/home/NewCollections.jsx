8b3bda875b5bd222072ac89e07bbba9cc81b1b17
Sameer Alshamiri <sameer.alshamiri3@gmail.com>
2025-05-24 17:51:08 -0400

    added dynamic values and loading state

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -1,39 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 export default function NewCollections() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://remote-internship-api-production.up.railway.app/newCollections"
+        );
+        setData(response.data.data);
+        console.log(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error occured in fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <section id="new-collections">
       <div className="container">
         <div className="row">
           <h2 className="new-collections__title">New Collections</h2>
           <div className="new-collections__body">
-            {new Array(6).fill(0).map((_, index) => (
-              <div className="collection-column">
-                <Link to="/collection" key={index} className="collection">
-                  <img
-                    src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
-                    alt=""
-                    className="collection__img"
-                  />
-                  <div className="collection__info">
-                    <h3 className="collection__name">Bored Ape Kennel Club</h3>
-                    <div className="collection__stats">
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">Floor</span>
-                        <span className="collection__stat__data">0.46 ETH</span>
-                      </div>
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">
-                          Total Volume
-                        </span>
-                        <span className="collection__stat__data">281K ETH</span>
+            <Swiper modules={[Navigation]} navigation spaceBetween={20} slidesPerView={6}  loop={true}>
+              {data.map((nft, index) => (
+                <SwiperSlide key={index}>
+                  <Link
+                    to={`/collection/${nft.collectionId}`}
+                    key={index}
+                    className="collection"
+                  >
+                    <img
+                      src={nft.imageLink}
+                      alt=""
+                      className="collection__img"
+                    />
+                    <div className="collection__info">
+                      <h3 className="collection__name">{nft.title}</h3>
+                      <div className="collection__stats">
+                        <div className="collection__stat">
+                          <span className="collection__stat__label">Floor</span>
+                          <span className="collection__stat__data">
+                            {Number(nft.floor).toFixed(2)} ETH
+                          </span>
+                        </div>
+                        <div className="collection__stat">
+                          <span className="collection__stat__label">
+                            Total Volume
+                          </span>
+                          <span className="collection__stat__data">
+                            {nft.totalVolume} ETH
+                          </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
