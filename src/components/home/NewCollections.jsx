7ad623229c82741693f882097efd03f9fceb6b2e
Sameer Alshamiri <sameer.alshamiri3@gmail.com>
2025-05-26 05:54:54 -0400

    skeleton loading state && dynamic values

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -5,10 +5,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
+import Skeleton from "../ui/Skeleton";
 
 export default function NewCollections() {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,41 +33,82 @@ export default function NewCollections() {
         <div className="row">
           <h2 className="new-collections__title">New Collections</h2>
           <div className="new-collections__body">
-            <Swiper modules={[Navigation]} navigation spaceBetween={20} slidesPerView={6}  loop={true}>
-              {data.map((nft, index) => (
-                <SwiperSlide key={index}>
-                  <Link
-                    to={`/collection/${nft.collectionId}`}
-                    key={index}
-                    className="collection"
-                  >
-                    <img
-                      src={nft.imageLink}
-                      alt=""
-                      className="collection__img"
-                    />
-                    <div className="collection__info">
-                      <h3 className="collection__name">{nft.title}</h3>
-                      <div className="collection__stats">
-                        <div className="collection__stat">
-                          <span className="collection__stat__label">Floor</span>
-                          <span className="collection__stat__data">
-                            {Number(nft.floor).toFixed(2)} ETH
-                          </span>
-                        </div>
-                        <div className="collection__stat">
-                          <span className="collection__stat__label">
-                            Total Volume
-                          </span>
-                          <span className="collection__stat__data">
-                            {nft.totalVolume} ETH
-                          </span>
+            <Swiper
+              modules={[Navigation]}
+              navigation
+              spaceBetween={20}
+              slidesPerView={6}
+              loop={true}
+            >
+              {loading
+                ? new Array(9).fill(0).map((_, index) => (
+                    <SwiperSlide key={`skeleton-${index}`}>
+                      <div className="collection">
+                        <figure className="collection__img">
+                          <Skeleton width="100%" height="150px" />
+                        </figure>
+                        <div className="collection__info">
+                          <h3 className="collection__name">
+                            <Skeleton width="100px" height="16px" />
+                          </h3>
+                          <div className="collection__stats">
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">
+                                <Skeleton width="60px" height="14px" />
+                              </span>
+                              <span className="collection__stat__data">
+                                <Skeleton width="80px" height="14px" />
+                              </span>
+                            </div>
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">
+                                <Skeleton width="60px" height="14px" />
+                              </span>
+                              <span className="collection__stat__data">
+                                <Skeleton width="80px" height="14px" />
+                              </span>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                </SwiperSlide>
-              ))}
+                    </SwiperSlide>
+                  ))
+                : data.map((nft, index) => (
+                    <SwiperSlide key={index}>
+                      <Link
+                        to={`/collection/${nft.collectionId}`}
+                        key={index}
+                        className="collection"
+                      >
+                        <img
+                          src={nft.imageLink}
+                          alt=""
+                          className="collection__img"
+                        />
+                        <div className="collection__info">
+                          <h3 className="collection__name">{nft.title}</h3>
+                          <div className="collection__stats">
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">
+                                Floor
+                              </span>
+                              <span className="collection__stat__data">
+                                {Number(nft.floor).toFixed(2)} ETH
+                              </span>
+                            </div>
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">
+                                Total Volume
+                              </span>
+                              <span className="collection__stat__data">
+                                {nft.totalVolume} ETH
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </SwiperSlide>
+                  ))}
             </Swiper>
           </div>
         </div>
