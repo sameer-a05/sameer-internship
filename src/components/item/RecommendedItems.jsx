ed2e3c6ac6c7ef5b93b366d47bed41747d3a802b
Sameer Alshamiri <sameer.alshamiri3@gmail.com>
2025-06-03 19:53:27 -0400

    implemented loading and interactivity with itemsPage and recommendedItems

--- src/components/item/RecommendedItems.jsx
+++ src/components/item/RecommendedItems.jsx
@@ -1,9 +1,74 @@
 import { faShoppingBag, faTableCells } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "../ui/Skeleton";
+import { SwiperSlide, Swiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
-export default function RecommendedItems() {
+export default function RecommendedItems({ collectionId, currentItemId }) {
+  if (!collectionId) {
+    return (
+      <div style={{ padding: "2rem 0" }}>
+        <Swiper
+          modules={[Navigation]}
+          navigation
+          spaceBetween={10}
+          loop={true}
+          slidesPerView={6}
+        >
+          {new Array(6).fill(0).map((_, index) => (
+            <SwiperSlide key={`placeholder-${index}`}>
+              <div className="item-column">
+                <div className="item">
+                  <figure
+                    className="item__img__wrapper"
+                    style={{ minHeight: "200px" }}
+                  >
+                    <Skeleton width="100%" height="200px" />
+                  </figure>
+                  <div className="item__details">
+                    <span className="item__details__name">
+                      <Skeleton height="14px" width="80px" />
+                    </span>
+                    <span className="item__details__price">
+                      <Skeleton height="14px" width="80px" />
+                    </span>
+                    <span className="item__details__last-sale">
+                      <Skeleton height="14px" width="80px" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    );
+  }
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://remote-internship-api-production.up.railway.app/collection/${collectionId}`
+        );
+        setData(response.data.data.items);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error in fetching data: ", error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [collectionId]);
+
   return (
     <section id="recommended-items">
       <div className="container">
@@ -16,38 +81,91 @@ export default function RecommendedItems() {
               </h3>
             </div>
             <div className="recommended-items__body">
-              {new Array(6).fill(0).map((_, index) => (
-                <div className="item-column">
-                  <Link to={"/item"} key={index} className="item">
-                    <figure className="item__img__wrapper">
-                      <img
-                        src="https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384"
-                        alt=""
-                        className="item__img"
-                      />
-                    </figure>
-                    <div className="item__details">
-                      <span className="item__details__name">Meebit #0001</span>
-                      <span className="item__details__price">0.98 ETH</span>
-                      <span className="item__details__last-sale">
-                        Last sale: 7.45 ETH
-                      </span>
-                    </div>
-                    <div className="item__see-more">
-                      <button className="item__see-more__button">
-                        See More
-                      </button>
-                      <div className="item__see-more__icon">
-                        <FontAwesomeIcon icon={faShoppingBag} />
+              {loading ? (
+                <Swiper
+                  modules={[Navigation]}
+                  navigation
+                  spaceBetween={10}
+                  loop={true}
+                  slidesPerView={6}
+                >
+                  {new Array(7).fill(0).map((_, index) => (
+                    <SwiperSlide key={`skeleton-${index}`}>
+                      <div className="carousel-item-column">
+                        <div className="item">
+                          <figure
+                            className="item__img__wrapper"
+                            style={{ minHeight: "200px" }}
+                          >
+                            <Skeleton width="100%" height="200px" />
+                          </figure>
+                          <div className="item__details">
+                            <span className="item__details__name">
+                              <Skeleton height="14px" width="80px" />
+                            </span>
+                            <span className="item__details__price">
+                              <Skeleton height="14px" width="80px" />
+                            </span>
+                            <span className="item__details__last-sale">
+                              <Skeleton height="14px" width="80px" />
+                            </span>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              ) : (
+                <Swiper
+                  modules={[Navigation]}
+                  navigation
+                  spaceBetween={10}
+                  loop={true}
+                  slidesPerView={6}
+                >
+                  {data
+                    .filter((nft) => nft.itemId !== currentItemId)
+                    .slice(0, 12)
+                    .map((nft, index) => (
+                      <SwiperSlide key={nft.itemId || index}>
+                        <div className="carousel-item-column">
+                          <Link to={`/item/${nft.itemId}`} className="item">
+                            <figure className="item__img__wrapper">
+                              <img
+                                src={nft.imageLink}
+                                alt=""
+                                className="item__img"
+                              />
+                            </figure>
+                            <div className="item__details">
+                              <span className="item__details__name">
+                                {nft.title}
+                              </span>
+                              <span className="item__details__price">
+                                {nft.price} ETH
+                              </span>
+                              <span className="item__details__last-sale">
+                                Last sale: {nft.lastSale} ETH
+                              </span>
+                            </div>
+                            <div className="item__see-more">
+                              <button className="item__see-more__button">
+                                See More
+                              </button>
+                              <div className="item__see-more__icon">
+                                <FontAwesomeIcon icon={faShoppingBag} />
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
+                      </SwiperSlide>
+                    ))}
+                </Swiper>
+              )}
             </div>
             <div className="recommended-items__footer">
               <Link
-                to={"/collection"}
+                to={`/collection/${collectionId}`}
                 className="recommended-items__footer__button"
               >
                 View Collection
